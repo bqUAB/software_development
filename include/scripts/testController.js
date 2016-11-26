3cca9810b09b2f0ff90c2134840e27cff3fef140
Ali <furkanbiten@gmail.com>
2016-11-26 15:12:33 +0100

    First tests of AngularJS

---
+++ include/scripts/testController.js
@@ -0,0 +1,7 @@
+angular
+	.module('test');
+	.controller('testController', function($scope){
+		
+		$scope.hello = 'Hello world!';
+		
+	});
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!doctype html>
 <html>
-<script src="scripts/angular.min.js"></script>
-<script src="scripts/script.js"></script>
-<script src="scripts/example.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="include/scripts/angular.min.js"></script>
+<script src="include/scripts/script.js"></script>
+<script src="include/scripts/example.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/2.2.0/ui-bootstrap-tpls.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/2.2.0/ui-bootstrap.min.js"></script>
 
@@ -12,16 +11,12 @@
 		<h2>Which is your favorite?</h2>
      	<label ng-repeat="name in names" for="{{name}}">
        			{{name}}
-        <input type="radio"
-              ng-model="my.favorite"
-              ng-value="name"
-              id="{{name}}"
-              name="favorite">
+        <input type="radio" ng-model="my.favorite" ng-value="name" id="{{name}}" name="favorite">
      	</label>
    		<div>You chose {{my}}</div>
  	</div>
  
- <div ng-app="myModule" ng-controller="myController">
+ 	<div ng-app="myModule" ng-controller="myController">
 			{{ message }}
 		</div>
 
