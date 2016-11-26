ee5b8feef1d99265d2540a83331faf0236f41cd6
Byron Quezada <byron.quezada@e-campus.uab.cat>
2016-11-26 23:04:54 +0100

    Added a working app for picture taking and uploading.

---
+++ include/functions.php
@@ -0,0 +1,39 @@
+<?php
+function compress_pic($source_url, $destination_url, $quality) {
+
+	$info = getimagesize($source_url);
+
+	if ($info['mime'] == 'image/jpeg')
+		$image = imagecreatefromjpeg($source_url);
+
+		elseif ($info['mime'] == 'image/gif')
+		$image = imagecreatefromgif($source_url);
+
+		elseif ($info['mime'] == 'image/png')
+		$image = imagecreatefrompng($source_url);
+
+		imagejpeg($image, $destination_url, $quality);
+		return $destination_url;
+}
+
+/*// Not working
+function resize_pic($source_url, $destination_url, $quality) {
+	// The File
+	$filename = $source_url;
+	echo "Filename: $filename<br>";
+	$percent = $quality;
+	
+	// Get new dimensions
+	list($width, $height) = getimagesize($filename);
+	$new_width = $width * $percent;
+	$new_height = $height * $percent;
+	
+	// Resample
+	$image_p = imagecreatetruecolor($new_width, $new_height);
+	$destination_url = imagecreatefromjpeg($filename);
+	imagecopyresampled($image_p, $destination_url, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+}
+*/
+
+// Create function to name pictures
+?>
--- include/picture_saving.php
+++ include/picture_saving.php
@@ -1,21 +1,21 @@
-<?php
-
-//$uploaddir = 'D:/byron/Google Drive/A First Semester 2016-2017/Software Development and Quality Assurance/images/';
+<?php 
+//require_once 'functions.php';
 $uploaddir = '../images/';
-$uploadfile = $uploaddir . basename($_FILES['test_pic']['name']);
+$uploadpic = $uploaddir . basename($_FILES['test_pic']['name']);
 
 echo '<pre>';
-if (move_uploaded_file($_FILES['test_pic']['tmp_name'], $uploadfile)) {
+
+if (move_uploaded_file($_FILES['test_pic']['tmp_name'], $uploadpic)) {
 	echo "File is valid, and was successfully uploaded.\n";
 } else {
 	echo "Possible file upload attack!\n";
 }
 
+
 echo 'Here is some more debugging info:<br>';
 print_r($_FILES);
 
 print "</pre>";
-
-?> 
+?>
 <br>
-<img src="https://drive.google.com/drive/folders/0Bz-w9y2Nkcx4d2JPSFZJVWxvUU0?usp=sharing/image.jpg" width="512px">
+<img src='<?php echo $uploadpic ?>' width="200px">
