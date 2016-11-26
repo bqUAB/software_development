a5c8f9168e8b14adb420ba53a52b337b2043bdf1
Byron Quezada <byron.quezada@e-campus.uab.cat>
2016-11-26 16:34:50 +0100

    First backend development commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# software_development
-Web application for Software Development
+# Software Development
+This is a Web application for Software Development. A [wiki](https://github.com/bqUAB/software_development/wiki) is available with important information regarding the development of the project.
+
+To edit this file please follow [this](https://help.github.com/categories/writing-on-github/) guidelines.
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html ng-app>
+  <head>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+  </head>
+  <body>
+  	<label>Name:</label>
+	<input type="text" ng-model="yourName" placeholder="Enter a name here">
+	<hr>
+	<h1>Hello {{yourName}}!</h1>
+	<hr>
+	<form enctype="multipart/form-data" name="cloth" method="POST" action="include/picture_saving.php">
+	  	<p>Tomar una foto</p>
+	  	<input type="file" accept="image/*" name="test_pic">
+	  	<input type="submit" value="Upload">
+	  	
+	</form>
+  </body>
+</html>
